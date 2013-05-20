123185639c03c85c4aace33eb172ab8fc4b29d43
Martin Keogh <martin1keogh@gmail.com>
2013-05-20 20:36:20 +0200

    Ajout d'une valeur Equipe renvoyant un Symbol

--- src/main/scala/Joueur.scala
+++ src/main/scala/Joueur.scala
@@ -9,6 +9,11 @@ class Joueur(id:Int) {
 
   val idPartenaire = (id+2)%4
 
+  val Equipe = {
+    if (id%2==0) 'NS
+    else 'EO
+  }
+
   override def toString = id match {
     case 0 => "Sud" // le joueur
     case 1 => "Ouest"
