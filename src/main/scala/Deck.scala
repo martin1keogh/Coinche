56ca1dad186d0f19ae9b9a8a96f69ef45d9182a6
Martin Keogh <martin1keogh@gmail.com>
2013-05-20 15:37:13 +0200

    Suppression de la variable d'objet deck.
    Permet de supprimer les effets de bords.
    Changement des methodes affectees.

--- src/main/scala/Deck.scala
+++ src/main/scala/Deck.scala
@@ -8,32 +8,23 @@ import scala.util.Random
  * To change this template use File | Settings | File Templates.
  */
 object Deck {
-  var deck:List[Card]=List()
-
   val sortedDeck:List[Card] = List.tabulate(32){new Card(_)}
+
+  def newShuffledDeck:List[Card] = {
+    shuffle(sortedDeck)
+  }
 
   def shuffle(deck:List[Card]):List[Card] = {
     Random.shuffle(deck)
   }
 
-  def init() {
-    deck = shuffle(sortedDeck)
-  }
-
-  def empty() {
-    deck = List()
-  }
-
-  def distribution() = {
+  def distribution(deck:List[Card]) = {
     val ret =
       (deck.slice(0,3)++deck.slice(12,14)++deck.slice(20,23),
        deck.slice(3,6)++deck.slice(14,16)++deck.slice(23,26),
        deck.slice(6,9)++deck.slice(16,18)++deck.slice(26,29),
        deck.slice(9,12)++deck.slice(18,20)++deck.slice(29,32)
       )
-//  deck is now empty
-//  It'll be filled as the game goes on
-    deck = List()
     ret
   }
 
