94f49081dba8ff5fdcc214371fd1b11d679f44ee
Martin Keogh <martin1keogh@gmail.com>
2013-05-20 23:13:57 +0200

    creation d'un fihier pour tout ce qui touche au encheres

---
+++ src/main/scala/Enchere.scala
@@ -0,0 +1,84 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: martin
+ * Date: 20/05/13
+ * Time: 22:31
+ * To change this template use File | Settings | File Templates.
+ */
+
+case class Enchere(couleur:Int,contrat:Int,equipe:Symbol,coinche:Int){
+
+  def couleurToString:String = couleur match {
+    case 0 => "Pique"
+    case 1 => "Carreau"
+    case 2 => "Trefle"
+    case 3 => "Coeur"
+    case 4 => "Tout Atout"
+    case 5 => "Sans Atout"
+  }
+
+  def equipeToString:String = equipe match {
+    case 'NS => "Nord/Sud"
+    case 'EO => "Est/Ouest"
+  }
+
+  def coincheToString:String = coinche match {
+    case 1 => ""
+    case 2 => ", coinche!"
+    case 4 => ", coinche/sur-coinche!!"
+  }
+
+  override def toString = contrat+" a "+couleurToString+" pour "+equipeToString+coincheToString
+}
+
+
+object Enchere {
+  //TODO gerer les coinches
+  /**
+   *
+   * @return (couleur,contrat,equipe,coinche)
+   *         equipe : symbol 'NS ou 'EO
+   *         coinche : 1 = pas de coinche, 2 = coinche, 4 = contre
+   */
+  def enchere():Option[Enchere] = {
+
+    var ret:Option[Enchere] = None
+    var nbPasse = 0
+
+    def annonceLegal(a:Int):Boolean = {
+      val annonceCourante = ret.getOrElse(new Enchere(0,70,'FU,0)).contrat
+      //TODO gerer les capots/generales
+      (a%10 == 0 && a > annonceCourante && a < 170)
+    }
+
+    def annonceImpossible():Boolean = {
+      if (ret.isEmpty) false
+      else ret.get.contrat >= 160
+    }
+
+    def effectuerEnchere():Option[Enchere] = {
+      var ret:Option[Enchere] = None
+      val couleur = Reader.getCouleur
+      if (!(couleur == 0)) {
+        var contrat = -1
+        do contrat = Reader.getContrat while (!annonceLegal(contrat))
+        ret = Some(new Enchere(couleur-1,contrat,Partie.currentPlayer.Equipe,1))
+      }
+      ret
+    }
+
+    // Boucle principale lors des encheres
+    while ( (!annonceImpossible() && (nbPasse < 3) // apres 3 passes on finit les encheres// on arrete les annonces si on ne peut plus monter
+            || (ret == None && nbPasse == 3))){   // sauf s'il n'y a pas eu d'annonce,auquel cas on attend le dernier joueur
+      val enchere = effectuerEnchere()
+      if (enchere.isEmpty) nbPasse=nbPasse+1
+      else {
+        //une enchere a etait faite, on remet le nombre de passe a zero
+        nbPasse=0
+        ret = enchere
+      }
+      Partie.currentPlayer = Partie.nextPlayer(Partie.currentPlayer)
+    }
+    ret
+  }
+}
