110cf69560f8a2068504100be1c67254e5c31fd3
Martin Keogh <martin1keogh@gmail.com>
2013-06-02 21:53:59 +0200

    Ajout d'une valeur par defaut au multiplicateur coinche (1)

--- src/main/scala/GameLogic/Enchere.scala
+++ src/main/scala/GameLogic/Enchere.scala
@@ -10,7 +10,7 @@ import Main.Main
  * To change this template use File | Settings | File Templates.
  */
 
-case class Enchere(couleur:Int,contrat:Int,id:Int,coinche:Int){
+case class Enchere(couleur:Int,contrat:Int,id:Int,coinche:Int = 1){
 
   def couleurToString:String = couleur match {
     case 0 => "Pique"
@@ -44,18 +44,18 @@ object Enchere {
   val Reader = Main.Reader
 
   var listEnchere:List[Enchere] = List()
-  var ret:Option[Enchere] = None
+  var current:Option[Enchere] = None
   var nbPasse = 0
 
   def annonceLegal(a:Int):Boolean = {
-    val annonceCourante = ret.getOrElse(new Enchere(0,70,0,0)).contrat
+    val annonceCourante = current.getOrElse(new Enchere(0,70,0,1)).contrat
     //TODO gerer les capots/generales
     (a%10 == 0 && a > annonceCourante && a < 170)
   }
 
   def annonceImpossible():Boolean = {
-    if (ret.isEmpty) false
-    else ret.get.contrat >= 160
+    if (current.isEmpty) false
+    else (current.get.contrat >= 160 || current.get.coinche > 1)
   }
 
   def effectuerEnchere():Option[Enchere] = {
@@ -80,23 +80,23 @@ object Enchere {
    */
   def enchere():Option[Enchere] = {
     // On reinitialise les variables globales
-    ret = None
+    current = None
     nbPasse = 0
     listEnchere = List()
 
     // Boucle principale lors des encheres
     while ( (!annonceImpossible() && (nbPasse < 3) // apres 3 passes on finit les encheres// on arrete les annonces si on ne peut plus monter
-            || (ret == None && nbPasse == 3))){   // sauf s'il n'y a pas eu d'annonce,auquel cas on attend le dernier joueur
+            || (current == None && nbPasse == 3))){   // sauf s'il n'y a pas eu d'annonce,auquel cas on attend le dernier joueur
       Printer.tourJoueurEnchere(Partie.currentPlayer)
       val enchere = effectuerEnchere()
       if (enchere.isEmpty) nbPasse=nbPasse+1
       else {
         //une enchere a etait faite, on remet le nombre de passe a zero
         nbPasse=0
-        ret = enchere
+        current = enchere
       }
       Partie.currentPlayer = Partie.nextPlayer(Partie.currentPlayer)
     }
-    ret
+    current
   }
 }
