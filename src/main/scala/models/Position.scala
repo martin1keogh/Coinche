fbfe9c884996f801632c1a7d9a9e4f38fd188f5c
Martin Keogh <martin1keogh@gmail.com>
2018-08-04 15:37:15 +0200

    Add Position#startingFrom

--- src/main/scala/models/Position.scala
+++ src/main/scala/models/Position.scala
@@ -1,6 +1,7 @@
 package mk.coinche.models
 
 import enumeratum._
+import shapeless._, nat._
 
 sealed trait Position extends EnumEntry
 
@@ -11,4 +12,16 @@ object Position extends Enum[Position] {
   case object West  extends Position
   case object South extends Position
   case object East  extends Position
+
+  val list = Sized[List](North, West, South, East)
+
+  def startingFrom(p: Position): Sized[List[Position], _4] = {
+    val doubled: Sized[List[Position], _8] = list ++ list
+    p match {
+      case North => doubled.drop(0).take(4)
+      case West  => doubled.drop(1).take(4)
+      case South => doubled.drop(2).take(4)
+      case East  => doubled.drop(3).take(4)
+    }
+  }
 }
---
+++ src/test/scala/models/PositionSpecs.scala
@@ -0,0 +1,19 @@
+package mk.coinche.models
+
+import org.scalatest._
+
+import Position._
+import Position.{North => N, West => W, South => S, East => E}
+
+class PositionSpecs extends FunSpec {
+  describe("Positions") {
+    describe("#startingFrom") {
+      it("should return the 4 next players in the N>W>S>E order") {
+        assert(startingFrom(N).repr === N::W::S::E::Nil)
+        assert(startingFrom(W).repr === W::S::E::N::Nil)
+        assert(startingFrom(S).repr === S::E::N::W::Nil)
+        assert(startingFrom(E).repr === E::N::W::S::Nil)
+      }
+    }
+  }
+}
